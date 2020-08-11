496d9136d95b53cd051b5583cae36f5e41e5d205
Kenneth Chen <unusualkid@gmail.com>
2020-08-11 18:19:29 +0800

    Add styling to coverArt when url == null

--- lib/widgets/song_info.dart
+++ lib/widgets/song_info.dart
@@ -1,4 +1,6 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:jusst_challenge/utility/size_config.dart';
 
 class SongInfo extends StatefulWidget {
   final String url;
@@ -23,33 +25,56 @@ class _SongInfoState extends State<SongInfo> {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Container(
-            child: widget.url != ''
-                ? Image.network(
-                    widget.url,
-                  )
-                : AspectRatio(
+          Stack(
+            children: [
+              AspectRatio(
+                aspectRatio: 1,
+                child: Container(
+                  child: Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Theme.of(context).disabledColor,
+                  ),
+                  height: SizeConfig.safeBlockVertical * 50,
+                ),
+              ),
+              Container(
+                child: CachedNetworkImage(
+                  imageUrl: widget.url,
+                  imageBuilder: (context, imageProvider) => AspectRatio(
                     aspectRatio: 1,
                     child: Container(
-                      child: Center(child: CircularProgressIndicator()),
-                      color: Theme.of(context).disabledColor,
-                      width: double.infinity,
+                      height: SizeConfig.safeBlockVertical * 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: DecorationImage(
+                          image: imageProvider,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
                     ),
                   ),
+                ),
+              ),
+            ],
           ),
           Container(
             padding: EdgeInsets.symmetric(vertical: 15),
             child: widget.artist != ''
                 ? Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
                       Text(
                         widget.title,
                         style: Theme.of(context).textTheme.headline6,
                       ),
-                      Text(
-                        'by ' + widget.artist,
-                        style: Theme.of(context).textTheme.subtitle1,
+                      Container(
+                        padding: EdgeInsets.only(top: 5),
+                        child: Text(
+                          'by ' + widget.artist,
+                          style: Theme.of(context).textTheme.subtitle1,
+                        ),
                       ),
                     ],
                   )
