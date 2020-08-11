7fba968d539eb7188b8030551dc4a30edb026d15
Kenneth Chen <unusualkid@gmail.com>
2020-08-11 14:11:26 +0800

    Add keys for json

--- lib/utility/strings.dart
+++ lib/utility/strings.dart
@@ -1,9 +1,16 @@
 class Strings {
   static const appName = 'Jusst Challenge';
+
+  static const systemKey = 'system';
+  static const volumeKey = 'volume';
+  static const bluetoothKey = 'bluetooth';
+  static const playbackKey = 'playback';
+  static const playbackPositionKey = 'playbackPosition';
   static const metaDataKey = 'metadata';
-  static const coverArtKey = 'coverArt';
-  static const artistKey = 'artist';
   static const titleKey = 'title';
+  static const artistKey = 'artist';
+  static const coverArtKey = 'coverArt';
+  static const durationKey = 'duration';
 }
 
 class API {
