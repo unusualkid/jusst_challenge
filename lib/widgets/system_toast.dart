5800a075e44316ed863546d8d6a2693c97eaf681
Kenneth Chen <unusualkid@gmail.com>
2020-08-12 11:54:37 +0800

    Change SystemToast to stateless

--- lib/widgets/system_toast.dart
+++ lib/widgets/system_toast.dart
@@ -1,46 +1,28 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:jusst_challenge/utility/strings.dart';
 
-class SystemToast extends StatefulWidget {
-  final message;
+class SystemToast extends StatelessWidget {
+  final String systemState;
 
-  const SystemToast({Key key, @required this.message}) : super(key: key);
-
-  @override
-  _SystemToastState createState() => _SystemToastState();
-}
-
-class _SystemToastState extends State<SystemToast> {
-  bool hideToast = false;
+  const SystemToast({Key key, this.systemState}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    Timer(Duration(seconds: 3), () {
-      setState(() {
-        hideToast = true;
-      });
-    });
-
-    if (widget.message != SystemState.ready) {
-      if (!hideToast) {
-        return Center(
-          child: Container(
-            padding: EdgeInsets.all(10),
-            color: Colors.grey.withOpacity(0.7),
-            child: Text(
-              Strings.systemKey + ' ' + widget.message,
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: Theme.of(context).textTheme.headline6.fontSize,
-              ),
+    if (systemState != SystemState.ready) {
+      return Center(
+        child: Container(
+          padding: EdgeInsets.all(10),
+          color: Colors.grey.withOpacity(0.7),
+          child: Text(
+            Strings.systemKey + ' ' + systemState,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: Theme.of(context).textTheme.headline6.fontSize,
             ),
           ),
-        );
-      }
+        ),
+      );
     }
-    hideToast = false;
     return SizedBox.shrink();
   }
 }
